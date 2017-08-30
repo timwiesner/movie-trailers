3e53c405093fcd6324f3c338863638418493c728
Tim Wiesner <timothyjwiesner@gmail.com>
2017-08-29 20:41:06 -0500

    fix: Add semicolon

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ $(document).on('click', '.hanging-close, .modal-backdrop, .modal', function (eve
 });
 // Start playing the video whenever the trailer modal is opened
 $(document).on('click', '.movie-tile', function (event) {
-	var trailerYouTubeId = $(this).attr('data-trailer-youtube-id')
+	var trailerYouTubeId = $(this).attr('data-trailer-youtube-id');
 	var sourceUrl = 'http://www.youtube.com/embed/' + trailerYouTubeId + '?autoplay=1&html5=1';
 	$("#trailer-video-container").empty().append($("<iframe></iframe>", {
 		'id': 'trailer-video',
